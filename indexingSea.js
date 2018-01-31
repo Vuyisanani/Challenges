dbe62c73d84acee7a36d699ea1a622f529285b5c
Vuyisanani Meteni <vuyisanani.meteni@SAMAC133.local>
2018-01-31 16:02:10 +0200

    Indexing and Searching

---
+++ indexingSea.js
@@ -0,0 +1,51 @@
+const users = [{
+    "name": "Joe",
+    "companyId": "A2100",
+    "type": "Admin"
+  }, {
+    "name": "Jane",
+    "companyId": "A2100",
+    "type": "Moderator"
+  }, {
+    "name": "Smith",
+    "companyId": "A2100",
+    "type": "User"
+  }, {
+    "name": "Smith",
+    "companyId": "A2100",
+    "type": "User"
+  }, {
+    "name": "Rocket",
+    "companyId": "A3100",
+    "type": "Admin"
+  }, {
+    "name": "Rick",
+    "companyId": "A3100",
+    "type": "User"
+  }, {
+    "name": "Tim",
+    "companyId": "A4100",
+    "type": "Admin"
+  }]
+  
+  function orchestrateUsers(users) {
+    let result = {};
+  
+    users.forEach(user => {
+      if (result[user.type]) result[user.type].push(user.name);
+      else result[user.type] = [user.name];
+    });
+    return result;
+  }
+  console.log(orchestrateUsers(users));
+  
+  //second method
+  function searchUsers(orchestratedUsers, userTypes, property, value) {
+    //need help here
+    userTypes.forEach(userType => {
+      if (userTypes.find(userType => userType === "User")) {
+        console.log(userType);
+      }
+    });
+  }
+  
